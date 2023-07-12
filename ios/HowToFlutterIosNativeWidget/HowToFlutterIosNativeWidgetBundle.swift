f976babef0372505791712938e2108f8f26ccd82
Jan Armbrust <jan.armbrust@googlemail.com>
2023-07-12 21:41:44 +0200

    add widget extension

---
+++ ios/HowToFlutterIosNativeWidget/HowToFlutterIosNativeWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HowToFlutterIosNativeWidgetBundle.swift
+//  HowToFlutterIosNativeWidget
+//
+//  Created by Jan Armbrust on 12.07.23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HowToFlutterIosNativeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HowToFlutterIosNativeWidget()
+    }
+}
