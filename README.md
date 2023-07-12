80c1261ea8e165e026ddabc3730b074ce5ff0bc1
Jan Armbrust <jan.armbrust@googlemail.com>
2023-07-12 21:36:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# how_to_flutter_ios_native_widget
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/how_to_flutter_ios_native_widget/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.how_to_flutter_ios_native_widget
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
